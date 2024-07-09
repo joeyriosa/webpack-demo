30c10eb186aaeaf7c5b7a38fb0d36bb682c80e8a
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-07-09 22:03:22 +0800

    Add Webpack - Loading CSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,7 @@ function component() {
     // Lodash now imported  by this script.
     //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.textContent = myName('Cody');
+    element.classList.add('hello');
 
     return element;
 }
