fcf557e24e81c28c06ba8078d92225cf0c89bdad
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-07-11 10:38:14 +0800

    Removed dependencies in prep for next lesson

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import _ from 'lodash';
-import myName from './myName';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
 
-    // Lodash now imported  by this script.
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName('Cody');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
