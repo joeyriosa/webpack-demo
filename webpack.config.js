c9df01851bb37850019e17f3ecc0d271b1f04266
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-07-11 11:03:10 +0800

    Add sample code for Webpack Output Managment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },    
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Managment',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,    //This deletes all files in the dist folder before building.
     },
 };
